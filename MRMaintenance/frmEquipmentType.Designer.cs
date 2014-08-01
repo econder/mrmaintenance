4f093863fcac1c612b1426187505ebdb42bee171
Eric Conder <ericconder@yahoo.com>
2014-08-01 19:00:04 -0400

    Added Equipment Type form for adding/editing/removing equipment types.

--- MRMaintenance/frmEquipmentType.Designer.cs
+++ MRMaintenance/frmEquipmentType.Designer.cs
@@ -37,16 +37,119 @@ namespace MRMaintenance
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.btnClose = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnRemove = new System.Windows.Forms.Button();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.listType = new System.Windows.Forms.ListBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// btnClose
+			// 
+			this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnClose.Location = new System.Drawing.Point(431, 175);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(75, 23);
+			this.btnClose.TabIndex = 72;
+			this.btnClose.Text = "&Close";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Location = new System.Drawing.Point(349, 46);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+			this.btnUpdate.TabIndex = 71;
+			this.btnUpdate.Text = "&Update";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// btnRemove
+			// 
+			this.btnRemove.Location = new System.Drawing.Point(159, 147);
+			this.btnRemove.Name = "btnRemove";
+			this.btnRemove.Size = new System.Drawing.Size(75, 23);
+			this.btnRemove.TabIndex = 70;
+			this.btnRemove.Text = "&Remove";
+			this.btnRemove.UseVisualStyleBackColor = true;
+			this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(430, 46);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 69;
+			this.btnAdd.Text = "&Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// listType
+			// 
+			this.listType.FormattingEnabled = true;
+			this.listType.Location = new System.Drawing.Point(5, 20);
+			this.listType.Name = "listType";
+			this.listType.Size = new System.Drawing.Size(229, 121);
+			this.listType.TabIndex = 67;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(241, 20);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(264, 20);
+			this.txtName.TabIndex = 66;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(241, 5);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 20);
+			this.label2.TabIndex = 65;
+			this.label2.Text = "Type Name";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(5, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 20);
+			this.label1.TabIndex = 68;
+			this.label1.Text = "Equipment Types";
 			// 
 			// frmEquipmentType
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(554, 295);
+			this.ClientSize = new System.Drawing.Size(511, 202);
+			this.Controls.Add(this.btnClose);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnRemove);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.listType);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "frmEquipmentType";
-			this.Text = "frmEquipmentType";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Equipment Types";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.ListBox listType;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnRemove;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnClose;
 	}
 }
--- MRMaintenance/frmEquipmentType.cs
+++ MRMaintenance/frmEquipmentType.cs
@@ -1,15 +1,20 @@
 ﻿/***************************************************************************************************
- * Class:   MRDbConnection.cs
- * Created By: Eric Conder
- * Created On: 8/1/2014
+ * Class:   	frmEquipment.cs
+ * Created By: 	Eric Conder
+ * Created On: 	8/1/2014
  * 
  * Changes:
  * 
  *
  * *************************************************************************************************/
 using System;
+using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
+
+using MRMaintenance.BusinessAccess;
+using MRMaintenance.BusinessObjects;
+
 
 namespace MRMaintenance
 {
@@ -18,16 +23,116 @@ namespace MRMaintenance
 	/// </summary>
 	public partial class frmEquipmentType : Form
 	{
+		private EquipmentTypeBA typeBA;
+		private DataTable dt;
+		
+		
 		public frmEquipmentType()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
 			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+			typeBA = new EquipmentTypeBA();
+			
+			this.FillData();
+		}
+		
+		
+		private void FillData()
+		{
+			dt = typeBA.Load();
+			
+			//Bind types listbox
+			listType.DataSource = dt;
+			listType.DisplayMember = "typeName";
+			listType.ValueMember = "typeId";
+			
+			//Bind type name textbox
+			txtName.DataBindings.Add("Text", dt, "typeName", true, DataSourceUpdateMode.Never, "");
+		}
+		
+		
+		private void ResetControlBindings()
+		{
+			//Clear existing databindings for each control
+			txtName.DataBindings.Clear();
+			
+			//Clear and reload datatable
+			dt.Clear();
+			
+			try
+			{
+				//Load database and re-bind all the controls
+				this.FillData();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+		
+		
+		void btnAdd_Click(object sender, EventArgs e)
+		{
+			EquipmentType type = new EquipmentType();
+			type.Name = txtName.Text;
+			
+			try
+			{
+				typeBA.Insert(type);
+				
+				//Reload data
+				this.ResetControlBindings();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+		
+		
+		void btnUpdate_Click(object sender, EventArgs e)
+		{
+			EquipmentType type = new EquipmentType();
+			type.ID = (long)listType.SelectedValue;
+			type.Name = txtName.Text;
+			
+			try
+			{
+				typeBA.Update(type);
+				
+				//Reload data
+				this.ResetControlBindings();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+		
+		
+		void btnRemove_Click(object sender, EventArgs e)
+		{
+			EquipmentType type = new EquipmentType();
+			type.ID = (long)listType.SelectedValue;
+			type.Name = txtName.Text;
+			
+			try
+			{
+				typeBA.Delete(type);
+				
+				//Reload data
+				this.ResetControlBindings();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+		
+		
+		void btnClose_Click(object sender, EventArgs e)
+		{
+			this.Hide();
 		}
 	}
 }
